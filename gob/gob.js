d9baa320b03ea4bc7f19aa49a491e515ab44b566
Garen150 <128188660+Garen150@users.noreply.github.com>
2023-04-15 14:30:33 -0700

    p5

---
+++ gob/gob.js
@@ -0,0 +1,66 @@
+function setup() {
+createCanvas(800, 800);
+strokeWeight(2);
+}
+
+
+function draw() {
+ background(66, 212, 158);
+ fill(71, 230, 103);
+ rect(335, 500, 140, 300);
+ //torso//
+ 
+ rect(285, 570, 50, 90);
+ rect(475, 570, 50 ,90);
+ fill(250, 219, 162);
+ rect(285, 660, 50, 90);
+ rect(475, 660, 50, 90);
+ //arms//
+ 
+ fill(250, 219, 162);
+ ellipse(400, 400, 300, 360);
+ //face//
+ 
+ fill(204, 178, 131);
+ rect(385, 400, 30, 30);
+ //nose//
+ 
+ fill(255, 255, 255);
+ ellipse(340, 350, 50, 50);
+ fill(255, 255, 255);
+ ellipse(460, 350, 50, 50);
+ fill(0);
+ ellipse(460, 350, 20, 20);
+ fill(0);
+ ellipse(340, 350, 20, 20);
+ //eyes//
+ ellipse(400, 500, 90, 20)
+ //mouth//
+ 
+ fill(66, 56, 30);
+ ellipse(300, 250, 50, 50);
+ ellipse(330, 230, 50, 50);
+ ellipse(360, 210, 50, 50);
+ ellipse(390, 210, 50, 50);
+ ellipse(420, 210, 50, 50);
+ ellipse(450, 220, 50, 50);
+ ellipse(480, 230, 50, 50);
+ ellipse(510, 250, 50, 50);
+ //hair//
+ 
+ noFill();
+ rect(300, 320, 80, 60);
+ rect(420, 320, 80 ,60);
+ fill(0);
+ rect(380, 345, 40, 3);
+ rect(265, 345, 35, 3);
+ rect(500, 345, 35, 3);
+ //glasses//
+ 
+ fill(66, 56, 30);
+ rect(320, 305, 40, 10);
+ rect(440, 305, 40 ,10);
+ //eyebrows//
+ 
+ 
+}
